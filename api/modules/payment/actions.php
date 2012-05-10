5f70713d71259d925a40cfa9f55cf6a7b8847bd1
Tom Cooksey <tom.cooksey@uk.ithrconsulting.com>
2012-05-10 19:23:56 +0100

    payment

--- api/modules/payment/actions.php
+++ api/modules/payment/actions.php
@@ -47,76 +47,39 @@ class payment {
 
         //file_put_contents('post.html', $this->getValuesWithKeys($_POST));
         //file_put_contents('get.html', $this->getValuesWithKeys($_GET));
-
-        error_reporting(E_ALL ^ E_NOTICE); 
-  
-        $header = ""; 
-        $emailtext = ""; 
-        // Read the post from PayPal and add 'cmd' 
-        $req = 'cmd=_notify-validate'; 
-        if(function_exists('get_magic_quotes_gpc')) {  
-            $get_magic_quotes_exists = true; 
-        } 
         
-        foreach ($_POST as $key => $value) {
-            if($get_magic_quotes_exists == true && get_magic_quotes_gpc() == 1){  
-                $value = urlencode(stripslashes($value)); 
-            } else { 
-                $value = urlencode($value); 
-            } 
-            $req .= "&$key=$value"; 
-        } 
-        // Post back to PayPal to validate 
-        $header .= "POST /cgi-bin/webscr HTTP/1.0\r\n"; 
-        $header .= "Content-Type: application/x-www-form-urlencoded\r\n"; 
-        $header .= "Content-Length: " . strlen($req) . "\r\n\r\n"; 
-        $fp = fsockopen ('ssl://www.paypal.com', 443, $errno, $errstr, 30); 
-        // Process validation from PayPal 
-        // TODO: This sample does not test the HTTP response code. All 
-        // HTTP response codes must be handled or you should use an HTTP 
-        // library, such as cUrl 
-            if (!$fp) { // HTTP ERROR
-                echo 'HTTP error';
-            } else {
-    
-                // NO HTTP ERROR 
-                fputs ($fp, $header . $req); 
-                while (!feof($fp)) { 
-                    $res = fgets ($fp, 1024);
-                    
-                    
         
-                    if (strcmp ($res, "VERIFIED") == 0) {
-                        
-                        die('here');
-                        
-                        // TODO: 
-                        // Check the payment_status is Completed 
-                        // Check that txn_id has not been previously processed 
-                        // Check that receiver_email is your Primary PayPal email 
-                        // Check that payment_amount/payment_currency are correct 
-                        // Process payment 
-                        // If 'VERIFIED', send an email of IPN variables and values to the 
-                        // specified email address 
-                        foreach ($_POST as $key => $value){ 
-                            $emailtext .= $key . " = " .$value ."\n\n"; 
-                        } 
-                        
-                        
-                        
-                    } else if (strcmp ($res, "INVALID") == 0) {
 
-                        // If 'INVALID', send an email. TODO: Log for manual investigation. 
-                        foreach ($_POST as $key => $value){ 
-                            $emailtext .= $key . " = " .$value ."\n\n"; 
-                        } 
-                        
-                    }  
-                }
+        $order = OrderQuery::create();
+        $order = $order->findPK($_POST['custom']);
+        
+        if($_POST['payment_status'] == 'complete') {
+            $order->setFulfilled(true);
+            $order->save();
+            
+            file_put_contents('get.html', 'saved');
+        }else{
+            if($order) {
+            
+                $orderSeats = OrderSeatQuery::create();
+                $orderSeats->filterByOrderId($order->getId());
                 
-                echo $emailtext;
-            } 
-        fclose ($fp);
+                $orderSeats = $orderSeats->find();
+                
+                if(count($orderSeats)) {
+                    foreach($orderSeats as $k => $v) {
+                        $v->delete();
+                    }
+                }
+               
+                
+                $order->delete();
+                
+                file_put_contents('get.html', 'deleted');
+            }
+        }
+        
+        
     
     }
     
